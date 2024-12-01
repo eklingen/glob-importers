d6acba650a0930eaf46d7aba1217bde6446a24d6
Elco Klingen <disillusion84@gmail.com>
2024-12-01 16:22:56 +0100

    Removed sass importer check for `fromImport` as it was causing issues with @use statements.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 
 # Changelog
 
+v1.0.4 - Removed sass importer check for `fromImport` as it was causing issues with @use statements.
 v1.0.3 - Small changes for Windows compatibility
 v1.0.2 - Fixed a bug where the ESBuild importer made import() calls instead of import statements.
 v1.0.1 - Added missing files in published set (oops)
--- importers/sass.js
+++ importers/sass.js
@@ -8,10 +8,10 @@ export default function globImporter(rootUrl = '') {
   rootUrl = resolvePath(process.cwd(), rootUrl)
 
   return {
-    canonicalize: async (url = '', options = { fromImport: false, containgUrl: {} }) => {
+    canonicalize: async (url = '') => {
       url = joinPath(rootUrl, url).replace(PREFIX, '')
 
-      return options.fromImport && glob.isDynamicPattern(url) ? new URL(url, PREFIX) : null
+      return glob.isDynamicPattern(url) ? new URL(url, PREFIX) : null
     },
 
     load: async (canonicalUrl) => {
